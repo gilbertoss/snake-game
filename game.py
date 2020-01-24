ae6d0f96565ced314c079c4d5d0baf11350f177d
gilbertoss <gilberto.souza@nyx.tc>
2020-01-24 16:13:44 +0000

    Adding snake food or better collision between objects.

--- game.py
+++ game.py
@@ -1,21 +1,40 @@
 import pygame, random
 from pygame.locals import *
 
+screen_width = 500
+screen_height = 700
+screen_grid = 10
+game_velocity = 5
+
+def location_random():
+    x = random.randint(0, screen_width - screen_grid)
+    y = random.randint(0, screen_width - screen_grid)
+    return (x//screen_grid * screen_grid, y//screen_grid * screen_grid)
+
+def collision(obj1, obj2):
+    return (obj1[0] == obj2[0]) and (obj1[1] == obj2[1])
+
 pygame.init()
-screen = pygame.display.set_mode((500, 700))
+
+screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Snake Game 1.0')
 
 snake = [(250, 350), (260, 350), (270, 350), (280, 350)]
-snake_body1 = pygame.Surface((10, 10))
+snake_body1 = pygame.Surface((screen_grid, screen_grid))
 snake_body1.fill((0, 0, 0))
-snake_body2 = pygame.Surface((10, 10))
-snake_body2.fill((255, 0, 0))
+snake_body2 = pygame.Surface((screen_grid, screen_grid))
+snake_body2.fill((107, 66, 38))
+
+apple_location = location_random()
+apple = pygame.Surface((screen_grid, screen_grid))
+apple.fill((255, 0, 0))
 
 direction = 'LEFT'
 clock = pygame.time.Clock()
 
+
 while True:
-    clock.tick(5)
+    clock.tick(game_velocity)
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
@@ -31,6 +50,11 @@ while True:
                 return switcher.get(i, "LEFT")
             direction = direction(event.key)
 
+    if collision(snake[0], apple_location):
+        apple_location = location_random()
+        snake.append((0, 0))
+        game_velocity += 1
+
     #makes the snake crawl
     for i in range(len(snake) - 1, 0, -1):
         snake[i] = (snake[i-1][0], snake[i-1][1])
@@ -45,6 +69,8 @@ while True:
     snake[0] = crawl(direction)
 
     screen.fill((255, 255, 255))
+
+    screen.blit(apple, apple_location)
 
     for i in range(len(snake)):
         if i == 0:
